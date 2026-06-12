be84b603f0b731f2dc13ea7d2ae21e59a67407a5
Lópezandresdavid340 <escobarlopezandresdavid85@gmail.com>
2026-06-11 21:10:24 -0500

    java-python

---
+++ funciones/funciones.js
@@ -0,0 +1,11 @@
+function saludar (){
+    console.log("Bievenidos a un saludo de funcion")
+}
+
+saludar();
+
+function sumar (x,y){
+    return x + y;
+}
+let resultado = sumar(5,3);
+console.log(resultado);
